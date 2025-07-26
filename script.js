b8b5b565991d7afcb5fb245808c009dcdde32f50
Farah-Stu <selma.salam45@gmail.com>
2025-07-26 10:07:37 +0100

    updating and editing function to render my episodes

--- script.js
+++ script.js
@@ -7,6 +7,33 @@ function setup() {
 function makePageForEpisodes(episodeList) {
   const rootElem = document.getElementById("root");
   rootElem.textContent = `Got ${episodeList.length} episode(s)`;
-}
 
+  episodeList.forEach((episode) => {
+    const section = document.createElement("section");
+    section.classList.add("episode-card");
+
+    const episodeCode= `S${String(episode.season).padStart(2, "0")}E${String(episode.number).padStart(2, "0")}`;
+    const codeElement = document.createElement("h3");
+    codeElement.textContent = episodeCode;
+    section.appendChild(codeElement);
+
+    const title = document.createElement("h2");
+    title.textContent = episode.name;
+
+    const currentSeason = document.createElement("h2");
+    currentSeason.textContent = `Season ${episode.season}`;
+
+    const image = document.createElement("img");
+    image.src = episode.image.medium;
+    const summary = document.createElement("p");
+    summary.textContent = episode.summary;
+
+    section.appendChild(title);
+    section.appendChild(currentSeason);
+    section.appendChild(summary);
+    section.appendChild(image);
+
+    rootElem.appendChild(section);
+  });
+}
 window.onload = setup;
