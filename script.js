ae12410aa18f8d5e68bd759b46d80393c88960e6
Farah-Stu <selma.salam45@gmail.com>
2025-08-01 15:35:55 +0100

    updating scripts

--- script.js
+++ script.js
@@ -6,32 +6,37 @@ function setup() {
 
 function makePageForEpisodes(episodeList) {
   const rootElem = document.getElementById("root");
-  rootElem.textContent = `Got ${episodeList.length} episode(s)`;
+  //rootElem.textContent = `Got ${episodeList.length} episode(s)`;
 
   episodeList.forEach((episode) => {
     const section = document.createElement("section");
     section.classList.add("episode-card");
 
-    const episodeCode= `S${String(episode.season).padStart(2, "0")}E${String(episode.number).padStart(2, "0")}`;
+    const episodeCode = `S${String(episode.season).padStart(2, "0")}E${String(
+      episode.number
+    ).padStart(2, "0")}`;
     const codeElement = document.createElement("h3");
     codeElement.textContent = episodeCode;
-    section.appendChild(codeElement);
+    // section.appendChild(codeElement);
 
     const title = document.createElement("h2");
-    title.textContent = episode.name;
+    title.textContent = `${episode.name} - ${episodeCode}`;
 
     const currentSeason = document.createElement("h2");
     currentSeason.textContent = `Season ${episode.season}`;
 
     const image = document.createElement("img");
     image.src = episode.image.medium;
-    const summary = document.createElement("p");
-    summary.textContent = episode.summary;
 
-    section.appendChild(title);
-    section.appendChild(currentSeason);
-    section.appendChild(summary);
+    const summary = document.createElement("div");
+    summary.innerHTML = episode.summary;
+
+     section.appendChild(title);
+    //section.appendChild(codeElement);
+
+    //section.appendChild(currentSeason);
     section.appendChild(image);
+    section.appendChild(summary);
 
     rootElem.appendChild(section);
   });
